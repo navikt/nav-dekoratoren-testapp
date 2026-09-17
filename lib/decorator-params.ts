950d0c67bb93c2942be8027522ac8dd45922fdf7
Tania Holst <holst.tania@gmail.com>
2026-09-17 15:01:11 +0200

    justerte params

--- lib/decorator-params.ts
+++ lib/decorator-params.ts
@@ -1,5 +1,5 @@
-const naisAppName = process.env.NAIS_APP_NAME ?? "nav-dekoratoren-testapp";
-const naisNamespace = process.env.NAIS_NAMESPACE ?? "personbruker";
+const naisAppName = process.env.NAIS_APP_NAME ?? "Fant ikke appnavn";
+const naisNamespace = process.env.NAIS_NAMESPACE ?? "Fant ikke namespace";
 
 export const teamName = `${naisAppName}.${naisNamespace}`;
 
